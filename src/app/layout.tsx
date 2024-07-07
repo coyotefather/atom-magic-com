cbd4aa71d85d12e014331082b344c2d5bfba8036
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-07 13:05:21 -0700

    Style updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header className={`${alegreya.className} flex z-10 justify-left`} />
+        <Header className={`${alegreya.className} flex z-10 justify-start sticky top-0`} />
         {children}
-        <Footer className={`${alegreya.className} flex z-10 justify-right`} />
+        <Footer className={`${alegreya.className} flex z-10 justify-end`} />
       </body>
     </html>
   );
