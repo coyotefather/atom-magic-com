a06f0acf117e0578e7f8c7d783158c6be77c420a
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-07 15:10:15 -0700

    Use a more semantic html container

--- src/app/components/global/Footer.tsx
+++ src/app/components/global/Footer.tsx
@@ -3,12 +3,12 @@ import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const Footer = ({font}) => {
 	return (
-		<div>
+		<footer>
 			<Nav
 				items={NAVIGATION_LINKS}
 				extended={true}
 				className={font + " flex z-10 justify-end"} />
-		</div>
+		</footer>
 	);
 };
 
