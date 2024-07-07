e03753aa5da6d13fd730d50dcd0f94effd8a3dd1
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-07 15:09:56 -0700

    Usea more semantic HTML element container

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -3,12 +3,12 @@ import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const Header = ({font}) => {
 	return (
-		<div className="sticky top-0">
+		<header className="sticky top-0">
 			<Nav
 				items={NAVIGATION_LINKS}
 				extended={false}
 				className={font + " flex z-10 justify-start"} />
-		</div>
+		</header>
 	);
 };
 
