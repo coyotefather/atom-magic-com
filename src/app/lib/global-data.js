1f2ea4149cc69e1cbf89147cba09a1ce706dd3a6
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-07 11:11:22 -0700

    Add missing comma

--- src/app/lib/global-data.js
+++ src/app/lib/global-data.js
@@ -2,6 +2,6 @@ export const NAVIGATION_LINKS = [
 	{ href: "/", name: "Home", icon: "", extended: false },
 	{ href: "https://atom-magic.com/codex", name: "Codex", icon: "", extended: false },
 	{ href: "/character", name: "Character Manager", icon: "", extended: false },
-	{ href: "/vorago", name: "Vorago", icon: "", extended: false }
+	{ href: "/vorago", name: "Vorago", icon: "", extended: false },
 	{ href: "/tools", name: "Vorago", icon: "", extended: true }
 ];
