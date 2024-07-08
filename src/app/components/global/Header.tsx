14c1d54122be0bf865a2f5808f35170b1a144eaa
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-08 10:04:30 -0700

    Update z-index and add logo images

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -3,11 +3,11 @@ import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const Header = ({font}) => {
 	return (
-		<header className="sticky top-0 bg-black">
+		<header className="sticky z-50 top-0 bg-black">
 			<Nav
 				items={NAVIGATION_LINKS}
 				extended={false}
-				className={font + " flex z-10 justify-start"} />
+				className={font + " flex justify-start"} />
 		</header>
 	);
 };
