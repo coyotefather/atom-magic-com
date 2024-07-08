12c74a5a4cf9d30027a8a3fc72572703dca7ed15
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-08 10:15:06 -0700

    Positioning updates

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -1,13 +1,24 @@
+import Image from 'next/image';
 import Nav from '@/app/components/common/Nav';
 import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const Header = ({font}) => {
 	return (
-		<header className="sticky z-50 top-0 bg-black">
-			<Nav
-				items={NAVIGATION_LINKS}
-				extended={false}
-				className={font + " flex justify-start"} />
+		<header className="sticky z-50 top-0 pt-2 pb-2 bg-black flex">
+			<div className="justify-start">
+				<Image
+			  	src="/AtomMagicLogoLight.svg"
+			  	alt="Atom Magic Logo"
+				className="inline-block"
+			  	width={431}
+			  	height={48}
+			  	priority
+				/>
+				<Nav
+					items={NAVIGATION_LINKS}
+					extended={false}
+					className={font + " inline-block"} />
+			</div>
 		</header>
 	);
 };
