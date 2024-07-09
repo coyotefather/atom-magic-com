eb5f9c2a9edcdbda041ba03fbcd0bd7a31ca2f25
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-09 09:32:32 -0700

    Add border

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 const Button = ({href, variant, children}) => {
-	let buttonColor = "bg-gold hover:bg-brightgold";
+	let buttonColor = "bg-gold hover:bg-brightgold border-black";
 	if(variant == "secondary") { buttonColor = "bg-black hover:bg-brightgold text-white" }
 	return (
 		<Link href={href}>
 		  <button
-			className={`${buttonColor} marcellus tracking-widest p-2 rounded-md`}>
+			className={`${buttonColor} marcellus tracking-widest p-2 pl-4 pr-4 rounded-md border-solid border-2`}>
 			{children}</button>
 		</Link>
 	);
