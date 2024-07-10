d79ab7ad7c62a1713836740637e63409cb4283ab
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-10 12:04:05 -0700

    Use Cards component

--- src/app/components/tools/Cards.tsx
+++ src/app/components/tools/Cards.tsx
@@ -1,7 +1,20 @@
 import Card from '@/app/components/common/Card';
 
 const Cards = () => {
-	return (<></>);
+	return (
+		<div className="mb-32 w-full grid grid-cols-2">
+			<Card
+			  image={ { src: "/Wheel_of_Cardinals.svg", alt: "Wheel of Cardinals", width: 500, height: 500 } }
+			  button={ { href: "/character", text: "Manage a Character", variant: "gradient" } }
+			  title="Character Manager"
+			  description="Build and manage a new character in minutes." />
+			<Card
+			  image={ { src: "/Wheel_of_Cardinals.svg", alt: "Wheel of Cardinals", width: 500, height: 500 } }
+			  button={ { href: "/vorage", text: "Play Vorago", variant: "gradient" } }
+			  title="Vorago"
+			  description="Play a game of Vorago with a friend." />
+		</div>
+	);
 };
 
 export default Cards;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Button from '@/app/components/common/Button';
-import Card from '@/app/components/common/Card';
+import Cards from '@/app/components/tools/Cards';
 
 export default function Home() {
   return (
@@ -9,18 +9,7 @@ export default function Home() {
         <h1>Title</h1>
         Header text
       </div>
-      <div className="mb-32 w-full grid grid-cols-2">
-        <Card
-          image={ { src: "/Wheel_of_Cardinals.svg", alt: "Wheel of Cardinals", width: 500, height: 500 } }
-          button={ { href: "/character", text: "Manage a Character", variant: "gradient" } }
-          title="Character Manager"
-          description="Build and manage a new character in minutes." />
-        <Card
-          image={ { src: "/Wheel_of_Cardinals.svg", alt: "Wheel of Cardinals", width: 500, height: 500 } }
-          button={ { href: "/vorage", text: "Play Vorago", variant: "gradient" } }
-          title="Vorago"
-          description="Play a game of Vorago with a friend." />
-      </div>
+      <Cards />
     </main>
   );
 }
