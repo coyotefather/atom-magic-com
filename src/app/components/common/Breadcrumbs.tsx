667c169e320dd9422c6f5765a27030478a84dcc9
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-11 12:58:33 -0700

    Create and add Breadcrumbs componnent

---
+++ src/app/components/common/Breadcrumbs.tsx
@@ -0,0 +1,8 @@
+const Breadcrumbs = () => {
+	return (
+		<div>
+		</div>
+	);
+};
+
+export default Breadcrumbs;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Breadcrumbs from '@/app/components/common/Breadcrumbs';
 import Header from '@/app/components/common/Header';
 import Cards from '@/app/components/home/Cards';
 
 export default function Home() {
   return (
     <main className="inconsolata container flex min-h-screen flex-col items-center justify-between p-24">
+      <Breadcrumbs />
       <Header title="Tools">
           Some text
       </Header>
