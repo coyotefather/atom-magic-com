fdccfdf66859d40ae0de97a43c22a17b238d00db
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-11 09:54:17 -0700

    Increase image size

--- src/app/components/home/Cards.tsx
+++ src/app/components/home/Cards.tsx
@@ -7,8 +7,8 @@ const Cards = () => {
 				image={ {
 					src: "/Wheel_of_Cardinals.svg",
 					alt: "Wheel of Cardinals",
-					width: 400,
-					height: 400,
+					width: 600,
+					height: 600,
 					priority: true } }
 			  button={ { href: "/character", text: "Manage a Character", variant: "gradient" } }
 			  title="Character Manager"
@@ -17,8 +17,8 @@ const Cards = () => {
 			  image={ {
 					src: "/vorago.png",
 					alt: "Vorago",
-					width: 400,
-					height: 400,
+					width: 600,
+					height: 600,
 					priority: true } }
 			  button={ { href: "/vorage", text: "Play Vorago", variant: "gradient" } }
 			  title="Vorago"
