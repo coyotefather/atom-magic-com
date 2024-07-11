b0d79ded3182abedd57c1d6d8b401e913c0f3953
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-10 18:25:42 -0700

    Use centered container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Cards from '@/app/components/home/Cards';
 
 export default function Home() {
   return (
-    <main className="inconsolata flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="inconsolata container flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Title</h1>
         Header text
