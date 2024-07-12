992cbf90c60780df499f7c611e56531c2e2a5e9f
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-12 09:32:22 -0700

    Fully define type of object of arrays

--- src/app/components/common/Breadcrumbs.tsx
+++ src/app/components/common/Breadcrumbs.tsx
@@ -9,7 +9,7 @@ const Breadcrumb = ({name, href}: { name: string, href: string }) => {
 	);
 };
 
-const Breadcrumbs = ({page, parents = []}: { page: string, parents: Array }) => {
+const Breadcrumbs = ({page, parents = []}: { page: string, parents: { name: string, href: string }[] }) => {
 	let breadcrumbs = (
 		<>
 			{parents.map((parent) => (
