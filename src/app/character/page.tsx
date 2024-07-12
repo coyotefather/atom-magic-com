be9b743f0abf3192a058b8e7e9d5403a7d5d1398
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-11 18:18:58 -0700

    Add character page route

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -1,5 +1,21 @@
+import Breadcrumbs from '@/app/components/common/Breadcrumbs';
+import Header from '@/app/components/common/Header';
+
 const Page = () => {
-	return <>Character Manager</>;
+	return (
+		<main className="inconsolata container flex min-h-screen flex-col items-center justify-between p-24 pt-12">
+		  <Breadcrumbs
+			  page="Character Manager"
+			  parents={[
+				  { href: "/tools", name: "Tools" }
+				]}
+		 />
+		  <Header title="Character Manager">
+			  Some text
+		  </Header>
+		  Coming soon!
+		</main>
+	);
 };
 
 export default Page;
