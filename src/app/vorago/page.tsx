108cf37dc8ca9986ff31b872a819ed29543b6d2a
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-12 08:57:21 -0700

    Update basic Vorago page

--- src/app/vorago/page.tsx
+++ src/app/vorago/page.tsx
@@ -1,5 +1,21 @@
+import Breadcrumbs from '@/app/components/common/Breadcrumbs';
+import Header from '@/app/components/common/Header';
+
 const Page = () => {
-	return <>Vorago</>;
+	return (
+		<main className="inconsolata container flex min-h-screen flex-col items-center justify-between p-24 pt-12">
+		  <Breadcrumbs
+			  page="Vorago"
+			  parents={[
+				  { href: "/tools", name: "Tools" }
+				]}
+		 />
+		  <Header title="Vorago">
+			  Coming soon!
+		  </Header>
+
+		</main>
+	);
 };
 
 export default Page;
