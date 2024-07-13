52f6c70e8eee8b9c517e420e03a08c3bbb57a8a7
Adam Butler <adam@instrumentumruinae.local>
2024-07-13 09:54:58 -0700

    Use DIV instead of P

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -7,7 +7,7 @@ const Header = ({
 	return (
 		<div className="w-full mt-4 mb-4">
 			<h1 className="w-full mb-2 text-2xl">{name}</h1>
-			<p className="w-full mb-2">{children}</p>
+			<div className="w-full mb-2">{children}</div>
 		</div>
 	);
 };
