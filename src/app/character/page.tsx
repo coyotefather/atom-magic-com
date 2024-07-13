2feb5a4831e4f92ebb8910c4ccdd22a53f756bb0
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-13 09:53:50 -0700

    Page formatting changes

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -1,19 +1,22 @@
 import Breadcrumbs from '@/app/components/common/Breadcrumbs';
 import Header from '@/app/components/common/Header';
+import Section from '@/app/components/common/Section';
 
 const Page = () => {
 	return (
-		<main className="inconsolata container flex min-h-screen flex-col items-center justify-between p-24 pt-12">
-		  <Breadcrumbs
-			  page="Character Manager"
-			  parents={[
-				  { href: "/tools", name: "Tools" }
-				]}
-		 />
-		  <Header name="Character Manager">
-			  Coming soon!
-		  </Header>
-
+		<main className="inconsolata container min-h-screen pt-12">
+			<Header name="Character Manager">
+				<Breadcrumbs
+					page="Character Manager"
+					parents={[
+						  { href: "/tools", name: "Tools" }
+					]}
+				/>
+  				Create your player character and start your journey across the world of Solum.
+			</Header>
+			<Section className="bg-black">
+  				Top section.
+			</Section>
 		</main>
 	);
 };
