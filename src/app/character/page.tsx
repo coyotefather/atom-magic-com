055678d3762ebc495a54830c1dc70b82d71db08a
Adam Butler <adam@instrumentumruinae.local>
2024-07-14 09:28:30 -0700

    Wrap children in div element

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -15,11 +15,13 @@ const Page = () => {
   				Create your player character and start your journey across the world of Solum.
 			</Header>
 			<Section variant="dark" name="The Basics">
+				<div>
   					<p>Enter and choose some basic information about your character. Note that none of the choices here affect scores. These are purely for roleplaying purposes.
 					</p>
 					<p>
   						For more information on cultures, see <a href="https://atom-magic.com/codex/cultures" title="Cultures on Solum">Cultures on Solum</a>.
 					</p>
+				</div>
 			</Section>
 		</main>
 	);
