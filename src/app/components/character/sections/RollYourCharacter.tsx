9b527c22d89963cfd656ab4e307b4d102e4b9440
Adam Butler <adam@instrumentumruinae.local>
2024-07-14 10:10:22 -0700

    Use Button component

--- src/app/components/character/sections/RollYourCharacter.tsx
+++ src/app/components/character/sections/RollYourCharacter.tsx
@@ -1,11 +1,16 @@
+import Button from '@/app/components/common/Button';
+
 const RollYourCharacter = () => {
 	return (
-		<div>
-			  <p>It's time to roll and set the base numbers for each stat. You can adjust the spread of points across each sub state for any stat category.
-			</p>
-			<p>
-				  <button>sdfsdf</button>
-			</p>
+		<div className="grid grid-flow-col auto-cols-auto">
+			<div>
+				It's time to roll and set the base numbers for each stat.<br />You can adjust the spread of points across each sub state for any stat category.
+			</div>
+			<div>
+				<Button
+				  href=""
+				  variant="gradient">Roll Character</Button>
+			</div>
 		</div>
 	);
 };
