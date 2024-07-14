90a59254785f66ff7725bd64aaa164a684459684
Adam Butler <adam@InstrumentumRuinae.local>
2024-07-14 09:10:27 -0700

    Section updates

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -14,8 +14,12 @@ const Page = () => {
 				/>
   				Create your player character and start your journey across the world of Solum.
 			</Header>
-			<Section className="bg-black">
-  				Top section.
+			<Section className="bg-black text-white" name="The Basics">
+  					<p>Enter and choose some basic information about your character. Note that none of the choices here affect scores. These are purely for roleplaying purposes.
+					</p>
+					<p>
+  						For more information on cultures, see <a href="https://atom-magic.com/codex/cultures" title="Cultures on Solum">Cultures on Solum</a>.
+					</p>
 			</Section>
 		</main>
 	);
--- src/app/components/common/Section.tsx
+++ src/app/components/common/Section.tsx
@@ -2,10 +2,11 @@ interface DivProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-const Section = ({className, children}: DivProps) => {
+const Section = ({name, className, children}: DivProps) => {
 	return (
-		<div className={`${className}`}>
+		<div className={`${className} pt-4 pb-4`}>
 			<div className="container">
+				<h2 className="marcellus text-xl">{name}</h2>
 				{children}
 			</div>
 		</div>
