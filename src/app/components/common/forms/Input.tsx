2eddec06595f5b2cbf06510c7056cd5620ab9b43
Adam Butler <adam@instrumentumruinae.local>
2024-07-15 15:26:19 -0700

    Create Input component

---
+++ src/app/components/common/forms/Input.tsx
@@ -0,0 +1,27 @@
+const Input = ({
+		name,
+		width = "auto",
+		required = false,
+	}: {
+		name: String,
+		width: String,
+		required: Boolean,
+	}) => {
+	let requiredElement = (<></>);
+	if(required) {
+		requiredElement = (
+			<p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+				This field is required.
+			</p>
+		);
+	}
+	return (
+		<label className="block">
+			<span className="block text-sm font-medium text-slate-700">{name}</span>
+			<input type="text" name={name} className={`peer form-input w-${width} rounded-md border-2 focus:ring-gold focus:border-gold`}/>
+			{requiredElement}
+		</label>
+	);
+};
+
+export default Input;
