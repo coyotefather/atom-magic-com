a243462e7cff84c5adda2be989bd5e45ba37228d
Adam Butler <adam@instrumentumruinae.local>
2024-07-15 15:33:19 -0700

    Use Input component

--- src/app/components/character/sections/TheBasics.tsx
+++ src/app/components/character/sections/TheBasics.tsx
@@ -1,3 +1,5 @@
+import Input from '@/app/components/common/forms/Input';
+
 const TheBasics = () => {
 	return (
 		<div>
@@ -8,24 +10,10 @@ const TheBasics = () => {
 				  For more information on cultures, see <a href="https://atom-magic.com/codex/cultures" title="Cultures on Solum">Cultures on Solum</a>.
 			</p>
 			<div className="flex justify-between mt-4">
-				<label className="block">
-					<span className="block text-sm font-medium text-slate-700">Name</span>
-					<input type="text" name="Name" className="peer form-input w-96 rounded-md border-2 focus:ring-gold focus:border-gold"/>
-					<p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-						Please enter a name.
-					</p>
-				</label>
-				<label className="block">
-					<span className="block text-sm font-medium text-slate-700">Age</span>
-					<input type="number" name="Age" className="peer form-input w-24 rounded-md border-2 focus:ring-gold focus:border-gold"/>
-					<p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-						Please enter an age.
-					</p>
-				</label>
-				<label className="block">
-					<span className="block text-sm font-medium text-slate-700">Pronouns</span>
-					<input type="text" name="Pronouns" className="form-input w-56 rounded-md border-2 focus:ring-gold focus:border-gold"/>
-				</label>
+				<Input name="Name" width="96" required={true} />
+				<Input name="Age" width="24" required={true} />
+				<Input name="Pronouns" width="96" required={false} />
+
 				<label className="block">
 					<span className="block text-sm font-medium text-slate-700">Culture</span>
 					<select name="Culture" className="peer form-select w-96 rounded-md border-2 focus:ring-gold focus:border-gold">
