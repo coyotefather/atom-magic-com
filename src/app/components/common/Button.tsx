afd144fdc5da2d95acaec4b0f7f96162e382641b
Adam Butler <adam@instrumentumruinae.local>
2024-07-15 22:04:00 -0700

    Only use Link element when href is present

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -4,23 +4,34 @@ import Link from 'next/link';
 const Button = ({
 		href = "", buttonFunction = () => { return; }, variant = "primary", children
 	}: {
-		href: URL,
+		href: String,
 		buttonFunction: Function,
 		variant: String,
 		children: React.ReactNode
 	}) => {
 	let buttonColor = "";
+	let button = (<></>);
 	if(variant == "primary") { buttonColor = "bg-gold hover:bg-brightgold"; }
 	if(variant == "secondary") { buttonColor = "bg-black hover:bg-brightgold text-white" }
 	if(variant == "gradient") { buttonColor = "gradient hover:bg-brightgold"; }
-	return (
-		<Link href={href}>
-		  <button
-		  	onClick={buttonFunction}
-			className={`${buttonColor} inconsolata uppercase tracking-widest p-2 pl-4 pr-4 rounded-full`}>
-			<strong>{children}</strong></button>
-		</Link>
-	);
+	if(href == "") {
+		button = (
+			<button
+				onClick={buttonFunction}
+				className={`${buttonColor} inconsolata uppercase tracking-widest p-2 pl-4 pr-4 rounded-full`}>
+				<strong>{children}</strong></button>
+		);
+	} else {
+		button = (
+			<Link href={href}>
+				<button
+					onClick={buttonFunction}
+					className={`${buttonColor} inconsolata uppercase tracking-widest p-2 pl-4 pr-4 rounded-full`}>
+					<strong>{children}</strong></button>
+			</Link>
+		);
+	}
+	return {button};
 };
 
 export default Button;
