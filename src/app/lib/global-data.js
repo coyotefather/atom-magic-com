ba6626f0ca2f6666acd39861a64d0afd6586d2a7
Adam Butler <adam@instrumentumruinae.local>
2024-07-17 12:47:50 -0700

    Nav updates

--- src/app/lib/global-data.js
+++ src/app/lib/global-data.js
@@ -1,6 +1,6 @@
 export const NAVIGATION_LINKS = [
 	{ href: "https://atom-magic.com/codex", name: "CODEX", icon: "", extended: false },
-	{ href: "/character", name: "CHARACTER MANAGER", icon: "", extended: false },
-	{ href: "/vorago", name: "VORAGO", icon: "", extended: false },
-	{ href: "/tools", name: "TOOLS", icon: "", extended: true }
+	{ href: "/character", name: "CHARACTER MANAGER", icon: "", extended: true },
+	{ href: "/vorago", name: "VORAGO", icon: "", extended: true },
+	{ href: "/tools", name: "TOOLS", icon: "", extended: false }
 ];
