925b60b26ba8c69e2b0eb787f1e084b405f7db49
Adam Butler <adam@instrumentumruinae.local>
2024-07-16 18:40:20 -0700

    Flesh out Score component

--- src/app/components/character/sections/scores/Score.tsx
+++ src/app/components/character/sections/scores/Score.tsx
@@ -4,7 +4,43 @@ const Score = ({
 		score: string,
 	}) => {
 	return (
-		<div>
+		<div className="w-full mb-4">
+			<h3 className="marcellus text-lg border-b-2 border-solid mb-4">
+				{score}
+				<span className="float-right font-bold">50</span>
+			</h3>
+			<div className="mb-4">
+				Subscore
+				<span className="float-right">50</span>
+				<div className="text-sm">Modifiers</div>
+			</div>
+			<div className="mb-4">
+				Subscore
+				<span className="float-right">50</span>
+				<div className="text-sm">Modifiers</div>
+			</div>
+			<div className="mb-4">
+				Subscore
+				<span className="float-right">50</span>
+				<div className="text-sm">Modifiers</div>
+			</div>
+			<div className="mb-4">
+				Subscore
+				<span className="float-right">50</span>
+				<div className="text-sm">Modifiers</div>
+			</div>
+			<div className="mt-2 mb-8">
+				Elective
+				<span className="float-right">5</span>
+			</div>
+			<div className="text-sm text-right">
+				<div>
+					{score} points available: 0/200
+				</div>
+				<div>
+				Elective points available: 0/20
+				</div>
+			</div>
 		</div>
 	);
 };
