689dc94d271280a613e79871ec2547bddf2a17ab
Adam Butler <adam@instrumentumruinae.local>
2024-07-19 20:49:17 -0700

    Add div for details

--- src/app/components/character/sections/ChoosePath.tsx
+++ src/app/components/character/sections/ChoosePath.tsx
@@ -4,21 +4,26 @@ import { PATHS } from '@/app/lib/global-data';
 
 const ChoosePath = () => {
 	return (
-		<div className="grid grid-cols-2 gap-4">
-			<div>
-				<p className="pb-2">
-					Paths indicate how your character came to be skilled in the arts of atom magic. While many techniques are common to all three, some are specific to each path.
-				</p>
-				<p>
-					For more information, see <a href="https://atom-magic.com/codex/cPaths" title="Paths">Character paths</a>.
-				</p>
+		<div>
+			<div className="grid grid-cols-2 gap-4">
+				<div>
+					<p className="pb-2">
+						Paths indicate how your character came to be skilled in the arts of atom magic. While many techniques are common to all three, some are specific to each path.
+					</p>
+					<p>
+						For more information, see <a href="https://atom-magic.com/codex/cPaths" title="Paths">Character paths</a>.
+					</p>
+				</div>
+				<div className="m-auto">
+					<Select
+						name="Path"
+						widthClass="w-96"
+						required={true}
+						options={PATHS} />
+				</div>
 			</div>
-			<div className="m-auto">
-				<Select
-					name="Path"
-					widthClass="w-96"
-					required={true}
-					options={PATHS} />
+			<div>
+				details
 			</div>
 		</div>
 	);
