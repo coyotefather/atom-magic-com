d95d8666c9b82a4aeacb2b9899d4a49e684de51d
Adam Butler <adam@instrumentumruinae.local>
2024-07-19 18:34:20 -0700

    Style updates

--- src/app/components/character/sections/ChoosePath.tsx
+++ src/app/components/character/sections/ChoosePath.tsx
@@ -1,24 +1,24 @@
 import Input from '@/app/components/common/forms/Input';
 import Select from '@/app/components/common/forms/Select';
-import { CULTURES } from '@/app/lib/global-data';
+import { PATHS } from '@/app/lib/global-data';
 
 const ChoosePath = () => {
 	return (
 		<div className="grid grid-cols-2 gap-4">
 			<div>
-				<p>
+				<p className="pb-2">
 					Paths indicate how your character came to be skilled in the arts of atom magic. While many techniques are common to all three, some are specific to each path.
 				</p>
 				<p>
 					For more information, see <a href="https://atom-magic.com/codex/cPaths" title="Paths">Character paths</a>.
 				</p>
 			</div>
-			<div className="align-middle">
+			<div className="m-auto">
 				<Select
 					name="Path"
 					widthClass="w-96"
 					required={true}
-					options={CULTURES} />
+					options={PATHS} />
 			</div>
 		</div>
 	);
