c83173fe7ae32d1ff7332e1a310c846bb4ce7dfa
Adam Butler <adam@instrumentumruinae.local>
2024-07-19 18:14:37 -0700

    Create RollGear component

---
+++ src/app/components/character/sections/RollGear.tsx
@@ -0,0 +1,11 @@
+const RollGear = () => {
+	return (
+		<div>
+			<p>
+				Every character starts with a basic gear kit and rolls additional unique items and wealth.
+			</p>
+		</div>
+	);
+};
+
+export default RollGear;
