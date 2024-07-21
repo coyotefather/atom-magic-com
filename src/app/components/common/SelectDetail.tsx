4786035d269d4bda326645b03eeb239da97554a9
Adam Butler <adam@instrumentumruinae.local>
2024-07-21 09:38:09 -0700

    Update structure to align with left half of section

--- src/app/components/common/SelectDetail.tsx
+++ src/app/components/common/SelectDetail.tsx
@@ -14,28 +14,42 @@ const SelectDetail = ({
 		disabled: boolean
 	}) => {
 	return (
-		<div className="flex justify-around transition-all duration-200 ease-linear">
-			<div className="mx-2 my-auto">
-				<Icon
-					path={iconPath}
-					className={clsx(
-					'inline ml-2',
-					{
-						'opacity-25': disabled === true,
-					})}
-					size={3}
-					horizontal
-					vertical />
-			</div>
+		<div>
 			<div className={clsx(
-			'mt-2',
+			'',
 			{
 				'hidden': disabled === true,
 			})}>
-				<h3 className="text-2xl border-b-2 mb-2">{name}</h3>
-				<div>
-					{description}
+				<h3 className="marcellus text-3xl border-b-2">{name}</h3>
+				<div className="flex justify-around">
+					<div className="mt-4">
+						{description}
+					</div>
+					<div className="mx-2">
+						<Icon
+							path={iconPath}
+							className={clsx(
+							'inline ml-2',
+							{
+								'opacity-25': disabled === true,
+							})}
+							size={5} />
+					</div>
 				</div>
+			</div>
+			<div className={clsx(
+			'',
+			{
+				'hidden': disabled === false,
+			})}>
+				<Icon
+					path={iconPath}
+					className={clsx(
+					'm-auto',
+					{
+						'opacity-25': disabled === true,
+					})}
+					size={5} />
 			</div>
 		</div>
 	);
