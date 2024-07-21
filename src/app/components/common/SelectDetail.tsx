d8d2edf6bf1651a53eb618b17122e1379636186a
Adam Butler <adam@instrumentumruinae.local>
2024-07-21 09:12:47 -0700

    Build out detail structure

--- src/app/components/common/SelectDetail.tsx
+++ src/app/components/common/SelectDetail.tsx
@@ -1,23 +1,37 @@
+'use client';
+import Icon from '@mdi/react';
+import clsx from 'clsx';
+
 const SelectDetail = ({
 		iconPath,
 		name,
-		description
+		description,
+		disabled
 	}: {
 		iconPath: string,
 		name: string,
-		description: string
+		description: string,
+		disabled: boolean
 	}) => {
 	return (
-		<div className="flex justify-around max-w-[800px] mx-auto my-8 transition-all duration-200 ease-linear">
+		<div className="flex justify-around transition-all duration-200 ease-linear">
 			<div className="mx-2 my-auto">
 				<Icon
 					path={iconPath}
-					className="inline ml-2"
+					className={clsx(
+					'inline ml-2',
+					{
+						'opacity-25': disabled === true,
+					})}
 					size={3}
 					horizontal
 					vertical />
 			</div>
-			<div className="m-2">
+			<div className={clsx(
+			'mt-2',
+			{
+				'hidden': disabled === true,
+			})}>
 				<h3 className="text-2xl border-b-2 mb-2">{name}</h3>
 				<div>
 					{description}
