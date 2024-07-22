3e2962b2553625895f2d187a102cd3dacb37af7e
Adam Butler <adam@instrumentumruinae.local>
2024-07-22 14:08:28 -0700

    Create ExternalLink component

---
+++ src/app/components/common/ExternalLink.tsx
@@ -0,0 +1,19 @@
+import Icon from '@mdi/react';
+import { mdiOpenInNew } from '@mdi/js';
+
+const ExternalLink = ({
+		href,
+		name
+	}: {
+		href: string,
+		name: string
+	}) => {
+	return (
+		<a href={href} target="_new">
+			{name}
+			<Icon className="ml-1 inline-block align-baseline" size={0.5} path={mdiOpenInNew} />
+		</a>
+	);
+};
+
+export default ExternalLink;
