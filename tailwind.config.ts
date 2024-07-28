320c5930d9e72743cc49dd4d99fcfbd28e96df3f
Adam Butler <adam@instrumentumruinae.local>
2024-07-28 16:53:34 -0700

    Add gradient background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "standard-gradient": "linear-gradient(85deg, rgb(211, 142, 39) 0%, rgb(208, 145, 37) 11%, rgb(205, 151, 35) 23%, rgb(202, 154, 33) 34%, rgb(197, 159, 32) 46%, rgb(192, 160, 33) 57%, rgb(186, 163, 33) 68%, rgb(180, 165, 34) 79%, rgb(177, 170, 37) 90%, rgb(171, 174, 41) 100%)",
       },
     },
     container: {
