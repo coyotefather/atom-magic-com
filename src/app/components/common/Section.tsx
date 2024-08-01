9143d1a1fef7834de3581c4f1c384b1cc3a0fed0
Adam Butler <adam@instrumentumruinae.local>
2024-08-01 08:32:17 -0700

    Add more conditional classes

--- src/app/components/common/Section.tsx
+++ src/app/components/common/Section.tsx
@@ -21,7 +21,13 @@ const Section = ({
 		<div className={clsx(
 			'relative overflow-y-visible',
 			{
-				'pt-16 pb-16 container': variant === 'dark' || variant === 'light'
+				'pt-16 pb-16 container': variant === 'light'
+			},
+			{
+				'bg-black text-white': variant === 'dark'
+			},
+			{
+				'bg-standard-gradient border-t-2': variant === 'gradient'
 			},
 			{
 				'border-t-2 border-black': variant === 'dual'
@@ -30,7 +36,7 @@ const Section = ({
 			{headTag}
 			{children}
 			<div className="w-[100px] h-[100px] absolute left-1/2 -bottom-[35px]">
-				<div className="bg-standard-gradient border-4 rounded-full w-[70px] h-[70px] grid grid-cols-1 content-center z-10 absolute -left-[35px] bottom-0">
+				<div className="bg-standard-gradient border-4 border-black rounded-full w-[70px] h-[70px] grid grid-cols-1 content-center z-10 absolute -left-[35px] bottom-0">
 					<NextImage
 					width={40}
 					height={40}
