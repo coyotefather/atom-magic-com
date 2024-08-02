697ea1f07c381a58d4b3996106f4bd906f2b5ecc
Adam Butler <adam@instrumentumruinae.local>
2024-08-02 12:31:47 -0700

    Change type to any[]

--- src/app/components/character/sections/scores/Scores.tsx
+++ src/app/components/character/sections/scores/Scores.tsx
@@ -3,7 +3,7 @@ import { SCORES, PATHS } from '@/app/lib/global-data';
 
 // hardcode path for now until store is built
 const curPath = PATHS.find((path) => path.value === "theurgist");
-let values: { [key: string]: number } = {};
+let values: { [key: string]: any[] } = {};
 if(curPath) {
 	curPath.modifiers.forEach( (score) => {
 		score.modifier.forEach( (m) => {
