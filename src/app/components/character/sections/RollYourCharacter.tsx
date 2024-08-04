bf81a2b26d485bbede17f33bee932c0face0690d
Adam Butler <adam@instrumentumruinae.local>
2024-08-04 09:36:09 -0700

    Add top border

--- src/app/components/character/sections/RollYourCharacter.tsx
+++ src/app/components/character/sections/RollYourCharacter.tsx
@@ -8,7 +8,7 @@ const RollYourCharacter = () => {
 	};
 
 	return (
-		<div className="grid grid-cols-2 w-full bg-standard-gradient">
+		<div className="grid grid-cols-2 w-full bg-standard-gradient border-t-2">
 			<div className="flex justify-end pt-16 pb-16">
 				<div className="max-w-[673px] pr-4">
 					<h2 className="marcellus text-3xl border-b-2 border-solid mb-4">Roll a Character?</h2>
