c8625e6e369ede7d42b6c1131a94aca18051a165
Adam Butler <adam@instrumentumruinae.local>
2024-08-05 08:59:38 -0700

    Add minimum height

--- src/app/components/global/Footer.tsx
+++ src/app/components/global/Footer.tsx
@@ -3,7 +3,7 @@ import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const Footer = () => {
 	return (
-		<footer className="bg-black">
+		<footer className="bg-black min-h-56">
 			<Nav
 				items={NAVIGATION_LINKS}
 				extended={true}
