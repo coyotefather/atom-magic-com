98ae767ebe87f75a93999b17d651a8d414155b7d
Adam Butler <adam@instrumentumruinae.local>
2024-08-06 09:44:21 -0700

    Add additional colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,15 +23,15 @@ const config: Config = {
         'adobe': '#bd6c48',
         'olive-green': '#BAB86C',
         'dark-olive-green': '#8A8841',
-        'goldenrod': '#daa520'
+        'goldenrod': '#daa520',
+        'sunset-blue': 'rgb(0, 179, 164)',
+        'sunset-red': 'rgb(238, 100, 58)'
       },
       backgroundImage: {
         'atom-magic-circle-white': "url('/atom-magic-circle-white.png')",
         'atom-magic-circle-black': "url('/atom-magic-circle-black.png')",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "standard-gradient": "linear-gradient(85deg, rgb(211, 142, 39) 0%, rgb(208, 145, 37) 11%, rgb(205, 151, 35) 23%, rgb(202, 154, 33) 34%, rgb(197, 159, 32) 46%, rgb(192, 160, 33) 57%, rgb(186, 163, 33) 68%, rgb(180, 165, 34) 79%, rgb(177, 170, 37) 90%, rgb(171, 174, 41) 100%)",
+        "sunset-gradient": "linear-gradient(195deg, rgb(0, 179, 164) 0%, rgb(66, 184, 145) 10%, rgb(104, 187, 123) 20%, rgb(141, 189, 107) 30%, rgb(175, 187, 93) 40%, rgb(208, 184, 88) 50%, rgb(215, 169, 71) 60%, rgb(221, 154, 60) 70%, rgb(227, 137, 53) 80%, rgb(234, 119, 52) 90%, rgb(238, 100, 58) 100%)",
       },
     },
     container: {
