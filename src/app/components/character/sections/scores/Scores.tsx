7f055dd4a95dfb9bb525f6d94b08d9ce57da32c8
Adam Butler <adam@instrumentumruinae.local>
2024-08-11 15:45:46 -0700

    Use store for path

--- src/app/components/character/sections/scores/Scores.tsx
+++ src/app/components/character/sections/scores/Scores.tsx
@@ -1,39 +1,49 @@
 import Score from '@/app/components/character/sections/scores/Score';
 import { SCORES, PATHS, GEAR } from '@/app/lib/global-data';
+import { useAppSelector, useAppDispatch } from '@/app/lib/hooks'
+import { setGear } from "@/app/lib/slices/characterSlice";
 
-// hardcode path for now until store is built
-const curPath = PATHS.find((path) => path.value === "theurgist");
-
-// hardcode gear until store is built
-const gear = {
-	weapon: GEAR.weapons['theurgist'].find((weapon) => weapon.key === "basicStaffOfEndurance"),
-	armor: GEAR.armor['theurgist'].find((weapon) => weapon.key === "basicCloakOfEndurance"),
-};
-
-let pathModifiersMap = new Map<string, number>([]);
-let gearModifiersMap = new Map<string, number>([]);
-
-if(curPath){
-	curPath.modifiers.forEach((score) =>  {
-		score.modifier.forEach((m) => {
-			pathModifiersMap.set(m.id, m.value);
-		});
-	});
-}
-
-if(gear.weapon && gear.armor) {
-	gear.weapon.modifiers.forEach((m) =>  {
-		gearModifiersMap.set(m.key, m.value);
-	});
-	gear.armor.modifiers.forEach((m) =>  {
-		let check = gearModifiersMap.get(m.key);
-		if(check) {
-			gearModifiersMap.set(m.key, (check + m.value));
-		}
-	});
-}
 
 const Scores = () => {
+	const path = useAppSelector(state => state.character.path);
+	const curPath = PATHS.find((p) => p.value === path);
+	const gear = useAppSelector(state => state.character.gear);
+	let pathModifiersMap = new Map<string, number>([]);
+	let gearModifiersMap = new Map<string, number>([]);
+
+	if(curPath){
+		curPath.modifiers.forEach((score) =>  {
+			score.modifier.forEach((m) => {
+				pathModifiersMap.set(m.id, m.value);
+			});
+		});
+	}
+
+	if(gear) {
+		gear.forEach((thisGear) => {
+			thisGear.modifiers.forEach((m) =>  {
+				let check = gearModifiersMap.get(m.key);
+				if(check) {
+					gearModifiersMap.set(m.key, (check + m.value));
+				} else {
+					gearModifiersMap.set(m.key, m.value);
+				}
+			});
+		});
+	}
+
+	// if(gear.weapon && gear.armor) {
+	// 	gear.weapon.modifiers.forEach((m) =>  {
+	// 		gearModifiersMap.set(m.key, m.value);
+	// 	});
+	// 	gear.armor.modifiers.forEach((m) =>  {
+	// 		let check = gearModifiersMap.get(m.key);
+	// 		if(check) {
+	// 			gearModifiersMap.set(m.key, (check + m.value));
+	// 		}
+	// 	});
+	// }
+
 	return (
 		<div className="grid grid-cols-2 divide-x-2 bg-white container">
 			<div className="pt-16 pb-16">
