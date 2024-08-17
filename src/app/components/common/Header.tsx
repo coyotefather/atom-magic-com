db42c79d7d4deb573c97373973a8965766d31d2a
Adam Butler <adam@instrumentumruinae.local>
2024-08-17 11:29:36 -0700

    Change to sunset gradient background

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -5,9 +5,11 @@ const Header = ({
 		children: React.ReactNode
 	}) => {
 	return (
-		<div className="container">
-			<h1 className="w-full mb-2 marcellus text-4xl">{name}</h1>
-			<div className="w-full mb-2">{children}</div>
+		<div className="bg-sunset-gradient border-b-2">
+			<div className="container pt-8 pb-8">
+				<h1 className="w-full mb-2 marcellus text-4xl">{name}</h1>
+				<div className="w-full mb-2">{children}</div>
+			</div>
 		</div>
 	);
 };
