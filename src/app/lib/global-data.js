4649ac6c06e1e4e93b991d74544e3008ebf34c37
Adam Butler <adam@instrumentumruinae.local>
2024-08-17 19:36:37 -0700

    Update navigation

--- src/app/lib/global-data.js
+++ src/app/lib/global-data.js
@@ -1,10 +1,10 @@
 import { mdiBookshelf, mdiScriptText, mdiHeadCog, mdiAtom } from '@mdi/js';
 
 export const NAVIGATION_LINKS = [
-	{ href: "https://atom-magic.com/codex", name: "CODEX", icon: "", extended: false },
-	{ href: "/character", name: "CHARACTER MANAGER", icon: "", extended: true },
-	{ href: "/vorago", name: "VORAGO", icon: "", extended: true },
-	{ href: "/tools", name: "TOOLS", icon: "", extended: false }
+	{ href: "/codex", name: "CODEX", icon: "", extended: false },
+	{ href: "/character", name: "CHARACTER MANAGER", icon: "", extended: false },
+	{ href: "/vorago", name: "VORAGO", icon: "", extended: false },
+	{ href: "/contact", name: "CONTACT", icon: "", extended: true }
 ];
 
 export const CULTURES = [
