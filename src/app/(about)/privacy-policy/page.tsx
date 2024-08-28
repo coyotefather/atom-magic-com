a3d50ac10a7a9b196f08a1cc02ab9d067276d71a
Adam Butler <adam@instrumentumruinae.local>
2024-08-28 09:58:27 -0700

    Add About routes and pages

---
+++ src/app/(about)/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/app/components/common/Header';
+const Page = () => {
+	return (
+		<main className="inconsolata">
+			<Header name="Privacy Policy">
+  				...
+			</Header>
+
+		</main>
+	);
+};
+
+export default Page;
