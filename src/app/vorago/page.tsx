67e3ed99ba442b018b08bdb4565756fe6be83948
Adam Butler <adam@instrumentumruinae.local>
2024-08-28 12:25:50 -0700

    Clean up base Vorago page

--- src/app/vorago/page.tsx
+++ src/app/vorago/page.tsx
@@ -1,21 +1,12 @@
 'use client';
-import {Breadcrumbs, BreadcrumbItem} from "@nextui-org/breadcrumbs";
 import Header from '@/app/components/common/Header';
-import Section from '@/app/components/common/Header';
 
 const Page = () => {
 	return (
-		<main className="inconsolata min-h-screen pt-12">
+		<main className="inconsolata">
 			<Header name="Vorago">
-				<Breadcrumbs>
-				  <BreadcrumbItem>Home</BreadcrumbItem>
-				  <BreadcrumbItem>Vorago</BreadcrumbItem>
-				</Breadcrumbs>
 				Coming soon!
 			</Header>
-			<Section name="Play Vorago">
-				Game board here.
-			</Section>
 		</main>
 	);
 };
