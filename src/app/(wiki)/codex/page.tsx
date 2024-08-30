2ceabf5ffb1ca00d9f5c3a1428bd19681ddab54d
Adam Butler <adam@instrumentumruinae.local>
2024-08-30 12:19:58 -0700

    Use unified CustomCard component

--- src/app/(wiki)/codex/page.tsx
+++ src/app/(wiki)/codex/page.tsx
@@ -1,22 +1,28 @@
-import Entries from "@/app/components/codex/Entries";
 import Header from '@/app/components/common/Header';
-import { sanityFetch } from "@/sanity/lib/client";
-import { ENTRIES_QUERY } from "@/sanity/lib/queries";
-import { ENTRIES_QUERYResult } from "../../../../sanity.types";
+import CustomCard from '@/app/components/common/CustomCard';
+// import { sanityFetch } from "@/sanity/lib/client";
+// import { ENTRIES_QUERY } from "@/sanity/lib/queries";
+// import { ENTRIES_QUERYResult } from "../../../../sanity.types";
 
 const Page = async () => {
 
-	const entries = await sanityFetch<ENTRIES_QUERYResult>({
-		query: ENTRIES_QUERY,
-	});
+	// const entries = await sanityFetch<ENTRIES_QUERYResult>({
+	// 	query: ENTRIES_QUERY,
+	// });
 
 	return (
 		<main className="inconsolata">
 			<Header name="Codex">
-				  Create your player character and start your journey across the world of Solum.
+				Lore, rules and more about Atom Magic.
 			</Header>
-			<div className="my-16">
-				<Entries entries={entries} />
+			<div className="my-16 container">
+				<CustomCard
+					type="category"
+					title="Lore"
+					description="Delve deep into the lore of Atom Magic."
+					url="/codex/categories/lore"
+					imagePath="/Wheel_of_Cardinals.svg"
+					showImage={true} />
 			</div>
 		</main>
 	);
