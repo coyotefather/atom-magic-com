79aa7f3c60188015fd0708c4287e6f0769a0dc91
Adam Butler <adam@instrumentumruinae.local>
2024-09-05 11:42:51 -0700

    Use inconsolata as font

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({
 		<div className="bg-sunset-gradient border-b-2">
 			<div className="container pt-8 pb-8">
 				<h1 className="w-full mb-2 marcellus text-4xl">{name}</h1>
-				<div className="w-full mb-2">{children}</div>
+				<div className="w-full mb-2 inconsolata">{children}</div>
 			</div>
 		</div>
 	);
