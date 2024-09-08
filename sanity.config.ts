2caa0802484b1fb3d2bd9ef3d1a07e9e40acde52
Adam Butler <adam@instrumentumruinae.local>
2024-09-08 11:44:11 -0700

    Add dependencies to use markdown

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,6 +8,7 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {colorInput} from '@sanity/color-input'
+import { markdownSchema } from "sanity-plugin-markdown";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
@@ -25,6 +26,7 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    markdownSchema(),
     colorInput(),
   ],
 })
