1bd2a0714cededac7d9870d3da690f882ce09194
Adam Butler <adam@instrumentumruinae.local>
2024-09-17 09:12:30 -0700

    Link logo to homepage

--- src/app/components/global/NavBar.tsx
+++ src/app/components/global/NavBar.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Nav from '@/app/components/common/Nav';
 import { NAVIGATION_LINKS } from '@/app/lib/global-data';
 
 const NavBar = () => {
 	return (
 		<header className="sticky z-50 top-0 p-2 bg-black flex flex-row items-center justify-between">
-			<Image
-				src="/AtomMagicLogoLight.svg"
-				alt="Atom Magic Logo"
-				className="inline-block my-2 ml-4"
-				width={215}
-				height={24}
-				priority
-				/>
+			<Link href="/">
+				<Image
+					src="/AtomMagicLogoLight.svg"
+					alt="Atom Magic Logo"
+					className="inline-block my-2 ml-4"
+					width={215}
+					height={24}
+					priority
+					/>
+			</Link>
 			<Nav
 				items={NAVIGATION_LINKS}
 				extended={false}
