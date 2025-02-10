3e7fdb494e58dcacba1bf5127bc353d6edaa77dc
Adam Butler <sakushi@gmail.com>
2025-02-10 10:51:42 -0800

    remove draftMode

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,59 +1,21 @@
 // ./src/sanity/client.ts
 
-import "server-only";
+import type { SanityClient } from 'next-sanity'
+import { createClient } from "@sanity/client";
 
-import { draftMode } from "next/headers";
-import { createClient, type QueryOptions, type QueryParams } from "next-sanity";
+import { apiVersion, dataset, projectId } from '../env'
 
-import { apiVersion, dataset, projectId } from "../env";
-import { token } from "./token";
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-  stega: {
-    enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === "preview",
-    studioUrl: "/studio",
-  },
-});
-
-export async function sanityFetch<QueryResponse>({
-  query,
-  params = {},
-  revalidate = 60,
-  tags = [],
-}: {
-  query: string;
-  params?: QueryParams;
-  revalidate?: number | false;
-  tags?: string[];
-}) {
-  const isDraftMode = draftMode().isEnabled;
-  if (isDraftMode && !token) {
-    throw new Error("Missing environment variable SANITY_API_READ_TOKEN");
-  }
-
-  let dynamicRevalidate = revalidate;
-  if (isDraftMode) {
-    // Do not cache in Draft Mode
-    dynamicRevalidate = 0;
-  } else if (tags.length) {
-    // Cache indefinitely if tags supplied, purge with revalidateTag()
-    dynamicRevalidate = false;
-  }
-
-  return client.fetch<QueryResponse>(query, params, {
-    ...(isDraftMode &&
-      ({
-        token: token,
-        perspective: "previewDrafts",
-        stega: true,
-      } satisfies QueryOptions)),
-    next: {
-      revalidate: dynamicRevalidate,
-      tags,
+export function getClient(previewToken?: string): SanityClient {
+  return createClient({
+    projectId,
+    dataset,
+    apiVersion,
+    useCdn: !previewToken,
+    perspective: previewToken ? 'previewDrafts' : 'published',
+    stega: {
+      enabled: previewToken ? true : false,
+      studioUrl: '/studio',
     },
-  });
+    token: previewToken
+  })
 }
