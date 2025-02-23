d346a76e812a0b09c85fc96e48d4241f1b03a942
Adam Butler <sakushi@gmail.com>
2025-02-23 11:22:08 -0800

    Add contact us page

---
+++ src/app/(website)/contact/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/app/components/common/Header';
+
+const Page = () => {
+	return (
+		<main className="notoserif">
+			<Header name="Disclaimers">
+  				Contact
+			</Header>
+			...
+		</main>
+	);
+};
+
+export default Page;
