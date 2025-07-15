12e8e1cd62a60715eafe384022245e715918aaf4
Adam Butler <sakushi@gmail.com>
2025-07-15 11:55:11 -0700

    Update type

--- src/sanity/schemaTypes/entryType.ts
+++ src/sanity/schemaTypes/entryType.ts
@@ -74,7 +74,7 @@ export const entryType = defineType({
       name: 'entryBody',
       title: 'Entry Body',
       options: {
-        imageUrl: (imageAsset: SanityImageAssetDocument) => `${imageAsset.url}?w=400&h=400`
+        imageUrl: (imageAsset: string) => `${imageAsset.url}?w=400&h=400`
       }
     })
   ],
