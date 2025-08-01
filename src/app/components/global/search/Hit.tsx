26c8d70511366fc2df8ce739634783c4b22a056a
Adam Butler <sakushi@gmail.com>
2025-08-01 10:59:27 -0700

    Handle null description

--- src/app/components/global/search/Hit.tsx
+++ src/app/components/global/search/Hit.tsx
@@ -23,7 +23,7 @@ const Hit = ({
 				</Link>
 			</div>
 			<div className="text-sm ml-8">
-				{hit.description.length > 200 ? hit.description.slice(0, 250) + '...' : hit.description}
+				{hit.description && hit.description.length > 200 ? hit.description.slice(0, 250) + '...' : hit.description}
 			</div>
 		</div>
 	);
