7fb384832eff36039424913b205ad08ed57a34bb
Adam Butler <sakushi@gmail.com>
2025-12-21 10:40:41 -0800

    Add “md” to config

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -2,9 +2,9 @@ const {heroui} = require("@heroui/react");
 
 const config = {
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,md,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,md,mdx}",
 		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
 	  ],
 	  theme: {
