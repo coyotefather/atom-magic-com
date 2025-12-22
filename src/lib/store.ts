9725764b35f71060ea01ee8254e3e2b81e46ed13
Adam Butler <sakushi@gmail.com>
2025-12-21 16:54:57 -0800

    Import and use Vorago slice

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,10 +1,12 @@
 import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit'
 import characterReducer from "@/lib/slices/characterSlice";
+import voragoReducer from "@/lib/slices/voragoSlice";
 
 export const makeStore = () => {
 	return configureStore({
 		reducer: {
 			character: characterReducer,
+			vorago: voragoReducer,
 		}
 	})
 }
