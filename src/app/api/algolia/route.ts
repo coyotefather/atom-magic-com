e90cc3b6c1cbb94c95cb261aef648513e7f778e8
Adam Butler <sakushi@gmail.com>
2025-12-30 16:58:28 -0800

    Use correct property

--- src/app/api/algolia/route.ts
+++ src/app/api/algolia/route.ts
@@ -125,7 +125,7 @@ export async function POST(request: Request) {
 		const updatedValue = {
 			_id: value._id,
 			title: value.title,
-			slug: value.slug,
+			slug: value.slug.current,
 			entryBody: value.entryBody?.slice(0, 9000),
 			description: value.description,
 			_type: value._type,
